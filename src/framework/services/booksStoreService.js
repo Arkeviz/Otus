3756924dbc838545e5d69cec42e04d5df38e0e89
Arkeviz <koposov@live.ru>
2026-06-08 14:27:07 +0300

    Task-8: чутка поправил сервис, в частности `baseURL` - под следующее задание

--- src/framework/services/booksStoreService.js
+++ src/framework/services/booksStoreService.js
@@ -2,7 +2,7 @@ import { ofetch } from 'ofetch'
 import { config as bookstoreConfig } from '@/framework/config/bookstore.js'
 
 const instance = ofetch.create({
-  baseURL: `${bookstoreConfig.baseURL}/Account/v1`,
+  baseURL: bookstoreConfig.baseURL,
   // Ставим флаг, чтобы при кодах 4ХХ и 5ХХ не вызывать ошибки и работать с ответами
   // https://github.com/unjs/ofetch/tree/v1#%EF%B8%8F-handling-errors
   ignoreResponseError: true,
@@ -10,33 +10,70 @@ const instance = ofetch.create({
 
 export const booksStoreService = {
   userController: {
-    createUser: params => instance.raw('/User', {
+    /**
+     * Создание пользователя
+     * @param {{ userName: string, password: string }} params
+     * @param options
+     */
+    createUser: (params, options = {}) => instance.raw('/Account/v1/User', {
       method: 'POST',
       body: params,
+      ...options,
     }),
-    // FIXME сделано чуть менее неудобно, т.к. нужно тестировать запросы в т.ч. без токенов
-    getUser: (userId, options = {}) => instance.raw(
-      `/User/${userId}`,
-      {
-        ...options,
-        method: 'GET',
-      },
-    ),
-    deleteUser: (userId, options = {}) => instance.raw(
-      `/User/${userId}`,
-      {
-        ...options,
-        method: 'DELETE',
-      },
-    ),
-    generateToken: params => instance.raw('/GenerateToken', {
-      method: 'POST',
-      body: { ...params },
-    }),
-    isUserAuthorized: params => instance.raw('/Authorized', {
-      method: 'POST',
-      body: { ...params },
-    }),
-  },
 
+    /**
+     * Получение пользователя
+     * @param {number} userId
+     * @param options
+     */
+    getUser: (userId, options = {}) => instance.raw(
+      `/Account/v1/User/${userId}`,
+      {
+        method: 'GET',
+        ...options,
+      },
+    ),
+
+    /**
+     * Удаление пользователя
+     * @param {number} userId
+     * @param options
+     */
+    deleteUser: (userId, options = {}) => instance.raw(
+      `/Account/v1/User/${userId}`,
+      {
+        method: 'DELETE',
+        ...options,
+      },
+    ),
+
+    /**
+     * Генерация токена для пользователя.
+     * Также, неявно, авторизует пользователя.
+     * @param {{ userName: string, password: string }} params
+     * @param options
+     */
+    generateToken: (params, options = {}) => instance.raw(
+      '/Account/v1/GenerateToken',
+      {
+        method: 'POST',
+        body: params,
+        ...options,
+      },
+    ),
+
+    /**
+     * Проверяет авторизован ли пользователь
+     * @param {{ userName: string, password: string }} params
+     * @param options
+     */
+    isUserAuthorized: (params, options = {}) => instance.raw(
+      '/Account/v1/Authorized',
+      {
+        method: 'POST',
+        body: params,
+        ...options,
+      },
+    ),
+  },
 }
