73483f79fc6983fd49226f28f711caabd15f1522
Arkeviz <koposov@live.ru>
2026-06-15 14:40:09 +0300

    Task-9: прокидываю .env в тесты через конфиг vite, т.к. `import.meta.env` не умеет работать в среде node

--- src/framework/config/bookstore.js
+++ src/framework/config/bookstore.js
@@ -1,7 +1,7 @@
 export const config = Object.freeze({
-  baseURL: import.meta.env.TEST_BOOKSTORE_API_URL ?? 'https://bookstore.demoqa.com',
-  userId: import.meta.env.TEST_BOOKSTORE_USER_ID,
-  username: import.meta.env.TEST_BOOKSTORE_USERNAME,
-  password: import.meta.env.TEST_BOOKSTORE_PASSWORD,
+  baseURL: import.meta.env.VITE_TEST_BOOKSTORE_API_URL,
+  userId: import.meta.env.VITE_TEST_BOOKSTORE_USER_ID,
+  username: import.meta.env.VITE_TEST_BOOKSTORE_USERNAME,
+  password: import.meta.env.VITE_TEST_BOOKSTORE_PASSWORD,
   passwordForTestUsers: 't3St-p@ssWor6!',
 })
--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,43 @@
+import process from 'node:process'
 import { fileURLToPath, URL } from 'node:url'
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // Configure Vitest (https://vitest.dev/config/)
-export default defineConfig({
-  test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
-    globals: false,
-    testTimeout: 10_000,
-    tags: [
-      {
-        name: 'Task-5',
-        description: 'Unit-тесты обычных функций',
-      },
-      {
-        name: 'Task-6',
-        description: 'Unit-тесты API работы с пользователем',
-      },
-      {
-        name: 'Task-8',
-        description: 'Unit-тесты API работы с пользователем',
-      },
-      {
-        name: 'Task-9',
-        description: 'Unit-тесты API работы с книгами',
-      },
-    ],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
 
-  // https://vitejs.dev/config/shared-options.html#resolve-alias
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+  return {
+    test: {
+      /* for example, use global to avoid globals imports (describe, test, expect): */
+      globals: false,
+      testTimeout: 10_000,
+      env,
+      tags: [
+        {
+          name: 'Task-5',
+          description: 'Unit-тесты обычных функций',
+        },
+        {
+          name: 'Task-6',
+          description: 'Unit-тесты API работы с пользователем',
+        },
+        {
+          name: 'Task-8',
+          description: 'Unit-тесты API работы с пользователем',
+        },
+        {
+          name: 'Task-9',
+          description: 'Unit-тесты API работы с книгами',
+        },
+      ],
     },
-  },
+
+    // https://vitejs.dev/config/shared-options.html#resolve-alias
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
